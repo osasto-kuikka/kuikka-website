7ec4e0aad411b14fc3518c6bec0f4032e77d8e71
nikolauska <nikolauska1@gmail.com>
2016-09-24 16:01:10 +0300

    Change KUIKKADB_USERNAME to KUIKKADB_USER

--- apps/kuikkadb/config/test.exs
+++ apps/kuikkadb/config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :kuikkadb, KuikkaDB.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: System.get_env("KUIKKADB_DB") || "kuikka_test",
-  username: System.get_env("KUIKKADB_USERNAME") || "postgres",
+  username: System.get_env("KUIKKADB_USER") || "postgres",
   password: System.get_env("KUIKKADB_PASSWORD") || "postgres",
   host: System.get_env("KUIKKADB_HOST") || "localhost",
   port: System.get_env("KUIKKADB_PORT") || "5432"
