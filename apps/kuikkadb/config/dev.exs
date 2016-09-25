e6c7b0ed73342d4c8b0438926de98a5d29d88569
nikolauska <nikolauska1@gmail.com>
2016-09-25 15:01:41 +0300

    Add pool/pool_size to kuikkdb configs

--- apps/kuikkadb/config/dev.exs
+++ apps/kuikkadb/config/dev.exs
@@ -5,4 +5,5 @@ config :kuikkadb, KuikkaDB.Repo,
   database: System.get_env("KUIKKADB_DB") || "kuikka_dev",
   username: System.get_env("KUIKKADB_USER") || "postgres",
   password: System.get_env("KUIKKADB_PASSWORD") || "postgres",
-  host: System.get_env("KUIKKADB_HOST") || "localhost"
+  host: System.get_env("KUIKKADB_HOST") || "localhost",
+  pool_size: 10
