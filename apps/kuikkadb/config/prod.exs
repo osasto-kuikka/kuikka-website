5bc9a4782189ca97c40aabea14ff508fcbcfed95
nikolauska <nikolauska1@gmail.com>
2016-09-25 16:00:22 +0300

    Change kuikkdb configs to use database url

--- apps/kuikkadb/config/prod.exs
+++ apps/kuikkadb/config/prod.exs
@@ -2,8 +2,6 @@ use Mix.Config
 
 config :kuikkadb, KuikkaDB.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: System.get_env("KUIKKADB_DB") || "kuikka_prod",
-  username: System.get_env("KUIKKADB_USER") || "postgres",
-  password: System.get_env("KUIKKADB_PASSWORD") || "postgres",
-  host: System.get_env("KUIKKADB_HOST") || "localhost",
+  url: System.get_env("KUIKKADB_URL") ||
+        "postgres://postgres:postgres@localhost/kuikka_prod",
   pool_size: 10
