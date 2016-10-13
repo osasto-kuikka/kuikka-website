eca24522e329feb8b152a71cbf10c4d2085daa37
Antiik91 <antiik91@gmail.com>
2016-10-13 10:57:50 +0300

    started working with default role id

--- apps/kuikkadb/lib/kuikkadb/schema/user.ex
+++ apps/kuikkadb/lib/kuikkadb/schema/user.ex
@@ -67,7 +67,11 @@ defmodule KuikkaDB.Schema.User do
 
   # TODO: Add default role
   defp add_default_role(changeset) do
-    changeset
+    when is_valid(KuikkaDB.Role.Changeset) do
+        drole_id = fetch_field(KuikkaDB.Role.Changeset, :role_id)
+        changeset = change(changeset, %{role_id: drole_id }
+    end
+
   end
 
   # TODO: Add default fireteam
