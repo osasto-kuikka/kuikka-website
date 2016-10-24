a8d020eb9ecf7ff2a113e81e0ff87ed6fb47c048
Antiik91 <antiik91@gmail.com>
2016-10-24 22:49:48 +0300

    delete testing for user

--- apps/kuikkadb/test/schema/user_test.exs
+++ apps/kuikkadb/test/schema/user_test.exs
@@ -6,10 +6,21 @@ defmodule KuikkaDBTest do
   test "new user" do
     UserSchema.new(%{username: "test", email: "test@test.com",
                       password: "test"})
-    user = UserSchema.one(username: "test")
+    user = UserSchema.struct(username: "test")
 
     assert user.username == "test"
     assert user.email == "test@test.com"
     assert user.imageurl == "http://test.osastokuikka.com/images/logo.svg"
+  end
+  test "delete user" do
+    UserSchema.new(%{username: "delete_this", email: "delete@delete.com",
+                      password: "test"})
+    delete_cantidate = UserSchema.one(username: "delete_this")
+
+    UserSchema.delete(delete_cantidate)
+
+    delete_cantidate = UserSchema.one(username: "delete_this")
+
+    assert delete_cantidate == nil
   end
 end
