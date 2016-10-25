9dbb5bd76b5c96cf6affb36fa337a8ab0efd5058
Antiik91 <antiik91@gmail.com>
2016-10-25 11:12:24 +0300

    little bit of refactoring the tests

--- apps/kuikkadb/test/schema/user_test.exs
+++ apps/kuikkadb/test/schema/user_test.exs
@@ -3,31 +3,29 @@ defmodule KuikkaDBTest do
 
   alias KuikkaDB.Schema.User, as: UserSchema
 
-  test "new user" do
-    UserSchema.new(%{username: "test", email: "test@test.com",
-                      password: "test"})
-    user = UserSchema.struct(username: "test")
 
+  setup do
+    UserSchema.new(%{username: "test", email: "test@test.com",
+                          password: "test"})
+    {:ok, user: UserSchema.one(username: "test")}
+   # {:ok, user_struct: UserSchema.struct(username: "test")}
+  end
+  test "new user", state do
+    user = UserSchema.struct(username: "test")
     assert user.username == "test"
     assert user.email == "test@test.com"
     assert user.imageurl == "http://test.osastokuikka.com/images/logo.svg"
   end
-  test "delete user" do
-    UserSchema.new(%{username: "delete_this", email: "delete@delete.com",
-                      password: "test"})
-    delete_cantidate = UserSchema.one(username: "delete_this")
+  test "delete user", state do
+    user = state[:user]
+    UserSchema.delete(user)
 
-    UserSchema.delete(delete_cantidate)
-
-    delete_cantidate = UserSchema.one(username: "delete_this")
-
-    assert delete_cantidate == nil
-  end
-  test "update user" do
-    UserSchema.new(%{username: "test", email: "test@test.com",
-                      password: "test"})
     user = UserSchema.one(username: "test")
 
+    assert user == nil
+  end
+  test "update user", state do
+    user = state[:user]
     user = UserSchema.update(user, %{username: "jokuAIVANmuu", email: "newmail@mail.com"})
     updated_user_struct = UserSchema.struct(username: "jokuAIVANmuu")
 
