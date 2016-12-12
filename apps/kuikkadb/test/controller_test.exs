07ac92df4d1c45aa6e51f3c7991a5766d9045349
nikolauska <nikolauska1@gmail.com>
2016-12-12 20:22:19 +0200

    more tests and insert functions for kuikkadb

--- apps/kuikkadb/test/controller_test.exs
+++ apps/kuikkadb/test/controller_test.exs
@@ -4,8 +4,36 @@ defmodule KuikkaDB.ControllerTest do
   """
   use KuikkaDB.TestCase
 
-  test "new user" do
-    assert {:ok, user} = KuikkaDB.new_user(123)
-    assert "#{user.steamid}" == "123"
+  setup do
+    {:ok, user} = KuikkaDB.new_user(76561197960435530)
+    {:ok, %{user: user}}
+  end
+
+  test "new user", %{user: user} do
+    assert "#{user.steamid}" == "76561197960435530"
+  end
+
+  test "get user", %{user: _} do
+    assert {:ok, got_user} = KuikkaDB.get_user(76561197960435530)
+    assert got_user.steamid == "76561197960435530"
+  end
+
+  test "get all users", %{user: _} do
+    assert {:ok, users} = KuikkaDB.get_all_users()
+    assert is_list(users)
+    assert length(users) > 0
+  end
+
+  test "new role", %{user: _} do
+    assert {:ok, _} = KuikkaDB.new_role("test", "test")
+  end
+
+  test "new fireteam", %{user: _} do
+    assert {:ok, _} = KuikkaDB.new_fireteam("test", "test")
+  end
+
+  test "new fireteam role", %{user: _} do
+    assert {:ok, _} = KuikkaDB.new_fireteam("test", "test")
+    assert {:ok, _} = KuikkaDB.new_fireteamrole("test", "test", false, "test")
   end
 end
