c00f5c38a41ffdead580c8cecad4dadab09c331f
nikolauska <nikolauska1@gmail.com>
2017-01-04 23:31:47 +0200

    Add calendar and changing next event date

--- apps/frontend/mix.exs
+++ apps/frontend/mix.exs
@@ -47,7 +47,8 @@ defmodule Frontend.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:steamex, "~> 0.0.6"}
+      {:steamex, "~> 0.0.6"},
+      {:calendar, "~> 0.16.1"},
     ]
   end
 end
