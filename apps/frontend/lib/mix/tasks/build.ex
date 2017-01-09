d90c26ebd6853a9c6f8d58599092f8f2b0ec8293
Niko Lehtovirta <niko.lehtovirta@hotmail.com>
2017-01-09 12:30:07 +0200

    Change travis test to erlang 19 and elixir 1.4 (#21)
    * Change travis test to erlang 19 and elixir 1.4
    
    * Add warning as error to compile, fix warnings

--- apps/frontend/lib/mix/tasks/build.ex
+++ apps/frontend/lib/mix/tasks/build.ex
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.Frontend.Build do
   Build npm packages for frontend.
   """
 
-  @shortdoc "Build npm packages and digest them for phoenix"
+  @doc "Build npm packages and digest them for phoenix"
   def run(_) do
     Mix.Shell.IO.cmd("cd apps/frontend && npm run deploy")
     Mix.Shell.IO.cmd("cd apps/frontend && mix phoenix.digest")
