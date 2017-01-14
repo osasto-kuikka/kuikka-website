48fb79afee18510d80e472c70f17ef61cec75cb4
nikolauska <nikolauska1@gmail.com>
2017-01-14 20:18:05 +0200

    Fix CI scripts to throw errors

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,6 +4,8 @@
 # This file is supposed to only run in travis
 # You should not try to run it locally
 
+set -o errexit -o nounset
+
 if [ "${TRAVIS_BRANCH}" == "staging" ]; then
   # Get deps and compile for test and prod release
   mix deps.get
