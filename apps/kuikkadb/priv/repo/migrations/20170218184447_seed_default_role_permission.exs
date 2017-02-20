13b9ea0ab7be73f095b1d16bceaecfb8f30a84b9
Antiik91 <antiik91@gmail.com>
2017-02-20 23:15:42 +0200

    Several trial and error attempts

--- apps/kuikkadb/priv/repo/migrations/20170218184447_seed_default_role_permission.exs
+++ apps/kuikkadb/priv/repo/migrations/20170218184447_seed_default_role_permission.exs
@@ -6,23 +6,23 @@ defmodule KuikkaDB.Repo.Migrations.SeedDefaultRolePermission do
   alias KuikkaDB.RolePermissions, as: RP
 
   def up do
-    {:ok, admin_permissions} = Permissions.get_all()
+#    {:ok, admin_permissions} = Permissions.get_all()
     insert_role_permissions("admin", Permissions.get_all())
   #  insert_role_permissions("user")
   end
 
   def down do
     {:ok, [admin]} = Roles.get(name: "admin")
-    RP.delete(id: admin.id)
+    RP.delete(role_id: admin.id)
   end
 
-  @spec insert_role_permissions(binary, binary | map) ::
-                                        {:ok, Ecto.Schema.t} | {:error, binary}
+#  @spec insert_role_permissions(binary, binary | list) ::
+#                                        {:ok, Ecto.Schema.t} | {:error, binary}
   defp insert_role_permissions(role_name, permissions = [perm | _])
                                                       when is_binary(perm) do
     {:ok, [role]} = Roles.get(name: role_name)
     for p <- permissions do
-      {:ok, [permission]} = Permissions.get(name: p)
+      {:ok, [permission]} = Permissions.get(name: p.name)
       {:ok, _} = RP.insert(role_id: role.id, permission_id: permission.id)
     end
   end
@@ -33,8 +33,8 @@ defmodule KuikkaDB.Repo.Migrations.SeedDefaultRolePermission do
                                                 when is_map(perm) do
     {:ok, [role]} = Roles.get(name: role_name)
     for p <- permissions do
-      {:ok, [permission]} = Permissions.get(name: p)
-      {:ok, _} = RP.insert(role_id: role.id, permission_id: permission.id)
+  #    {:ok, [permission]} = Permissions.get(name: p.name)
+      {:ok, _} = RP.insert(role_id: role.id, permission_id: p.id)
     end
   end
 end
